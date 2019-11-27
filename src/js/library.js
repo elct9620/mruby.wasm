0ee781098895cbc1b3a0c24e5c182a0c96234230
蒼時弦也 <elct9620@frost.tw>
2019-11-27 23:48:31 +0800

    Allow move sprite inside ruby

--- src/js/library.js
+++ src/js/library.js
@@ -1,21 +1,42 @@
-mergeInto(LibraryManager.library, {
-  extra: function(_unknown, mrb, self) {
-    console.log(arguments);
-    const inspect = ccall(
-      'inspect',
-      'string',
-      ['number', 'number'],
-      [mrb, self]
-    );
-    console.log(inspect)
-    return self;
+var LibraryPlugin = {
+  $Plugin__postset: 'Plugin.init()',
+  $Plugin: {
+    sprites: [],
+    init: function() {
+      window.Game = window.Game || {};
+      Game.AddSprite = function(sprite) {
+        return Plugin.sprites.push(sprite) - 1;
+      }
+    }
   },
-  get_attr: function(ptr, namePtr) {
-    const name = UTF8ToString(namePtr);
-    const ret = ObjectSpace.get_attr(ptr, name);
-    const bytes = lengthBytesUTF8(ret) + 1;
-    const heap = _malloc(bytes);
-    stringToUTF8(ret, heap, bytes);
-    return heap;
+  _sprite_exists__deps: ['$Plugin'],
+  _sprite_exists: function(idx) {
+    if (Plugin.sprites[idx]) {
+      return true;
+    }
+
+    return false;
+  },
+  _sprite_get_attr__deps: ['$Plugin'],
+  _sprite_get_int_attr: function(idx, attrNamePtr) {
+    var sprite = Plugin.sprites[idx]
+    if (!sprite) {
+      return;
+    }
+
+    var attrName = UTF8ToString(attrNamePtr);
+    return sprite[attrName];
+  },
+  _sprite_set_attr__deps: ['$Plugin'],
+  _sprite_set_int_attr: function(idx, attrNamePtr, value) {
+    var sprite = Plugin.sprites[idx]
+    if (!sprite) {
+      return;
+    }
+
+    var attrName = UTF8ToString(attrNamePtr);
+    sprite[attrName] = value;
   }
-});
+}
+
+mergeInto(LibraryManager.library, LibraryPlugin);
--- src/mruby.c
+++ src/mruby.c
@@ -1,8 +1,14 @@
 #include<mruby.h>
 #include<mruby/compile.h>
 #include<mruby/string.h>
-#include <mruby/variable.h>
+#include<mruby/variable.h>
 #include<emscripten.h>
+
+// From JS - Begin
+extern mrb_bool _sprite_exists(mrb_int ptr);
+extern mrb_int _sprite_get_int_attr(mrb_int ptr, const char* attr_name);
+extern void _sprite_set_int_attr(mrb_int ptr, const char* attr_name, mrb_int value);
+// From JS - End
 
 typedef struct {
   mrb_state* state;
@@ -10,6 +16,7 @@ typedef struct {
 
 static Plugin plugin;
 struct RClass* Game;
+struct RClass* Sprite;
 
 mrb_value game_update(mrb_state* mrb, mrb_value self) {
   mrb_value block;
@@ -23,7 +30,7 @@ mrb_value game_update(mrb_state* mrb, mrb_value self) {
     callback = mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "_update"));
     if (!mrb_nil_p(callback)) {
       mrb_value args[1];
-      args[0] = mrb_float_value(mrb,deltaTime);
+      args[0] = mrb_float_value(mrb, deltaTime);
       mrb_yield_argv(mrb, callback, 1, args);
     }
   } else {
@@ -33,6 +40,56 @@ mrb_value game_update(mrb_state* mrb, mrb_value self) {
   return mrb_nil_value();
 }
 
+mrb_value sprite_from_js(mrb_state* mrb, mrb_value self) {
+  mrb_int ptr;
+  mrb_get_args(mrb, "i", &ptr);
+
+  if (_sprite_exists(ptr)) {
+    mrb_value args[1] = { mrb_fixnum_value(ptr) };
+    return mrb_obj_new(mrb, Sprite, 1, args);
+  }
+
+  return mrb_nil_value();
+}
+
+mrb_value sprite_init(mrb_state* mrb, mrb_value self) {
+  mrb_int ptr;
+  mrb_get_args(mrb, "i", &ptr);
+  mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "_ptr"), mrb_fixnum_value(ptr));
+
+  return self;
+}
+
+mrb_value sprite_get_x(mrb_state* mrb, mrb_value self) {
+  mrb_int ptr = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "_ptr")));
+  mrb_int x = _sprite_get_int_attr(ptr, "x");
+  return mrb_fixnum_value(x);
+}
+
+mrb_value sprite_get_y(mrb_state* mrb, mrb_value self) {
+  mrb_int ptr = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "_ptr")));
+  mrb_int y = _sprite_get_int_attr(ptr, "y");
+  return mrb_fixnum_value(y);
+}
+
+mrb_value sprite_set_x(mrb_state* mrb, mrb_value self) {
+  mrb_int value;
+  mrb_get_args(mrb, "i", &value);
+
+  mrb_int ptr = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "_ptr")));
+  _sprite_set_int_attr(ptr, "x", value);
+  return mrb_fixnum_value(value);
+}
+
+mrb_value sprite_set_y(mrb_state* mrb, mrb_value self) {
+  mrb_int value;
+  mrb_get_args(mrb, "i", &value);
+
+  mrb_int ptr = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "_ptr")));
+  _sprite_set_int_attr(ptr, "y", value);
+  return mrb_fixnum_value(value);
+}
+
 EMSCRIPTEN_KEEPALIVE
 void plugin_init() {
   plugin.state = mrb_open();
@@ -40,6 +97,16 @@ void plugin_init() {
   // Game Module
   Game = mrb_define_module(plugin.state, "Game");
   mrb_define_class_method(plugin.state, Game, "update", game_update, MRB_ARGS_OPT(1)|MRB_ARGS_BLOCK());
+
+  // Sprite
+  Sprite = mrb_define_class(plugin.state, "Sprite", plugin.state->object_class);
+  mrb_undef_class_method(plugin.state, Sprite, "new");
+  mrb_define_class_method(plugin.state, Sprite, "[]", sprite_from_js, MRB_ARGS_REQ(1));
+  mrb_define_method(plugin.state, Sprite, "initialize", sprite_init, MRB_ARGS_REQ(1));
+  mrb_define_method(plugin.state, Sprite, "x", sprite_get_x, MRB_ARGS_NONE());
+  mrb_define_method(plugin.state, Sprite, "y", sprite_get_y, MRB_ARGS_NONE());
+  mrb_define_method(plugin.state, Sprite, "x=", sprite_set_x, MRB_ARGS_REQ(1));
+  mrb_define_method(plugin.state, Sprite, "y=", sprite_set_y, MRB_ARGS_REQ(1));
 }
 
 EMSCRIPTEN_KEEPALIVE
